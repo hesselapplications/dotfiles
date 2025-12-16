cf24fa08d3f9c3ffd48b6a66a180d2de0b014343
Noah Hessel <noah.hessel@datasite.com>
2025-12-16 13:57:13 -0700

    Tweaks

--- chrome/install.sh
+++ chrome/install.sh
@@ -4,13 +4,8 @@ BROWSER_EXTENSIONS_DIR="$HOME/Documents/Repos/browser-extensions"
 rm -rf "$BROWSER_EXTENSIONS_DIR"
 mkdir -p "$BROWSER_EXTENSIONS_DIR"
 git clone git@github.com:MerrillCorporation/browser-extensions.git "$BROWSER_EXTENSIONS_DIR"
-killall "Google Chrome" &> /dev/null || echo "Chrome not running."
-open -a "Google Chrome" --args \
-  --silent-launch \
-  --no-first-run \
-  --make-default-browser \
-  --load-extension="$BROWSER_EXTENSIONS_DIR/swagger-registry" \
-  --load-extension="$BROWSER_EXTENSIONS_DIR/swagger-auto-auth" \
-  --load-extension="$BROWSER_EXTENSIONS_DIR/github-squad-search"
 
-echo "Chrome setup complete. Remember to configure the auto-auth extension!"
+https://peter.sh/experiments/chromium-command-line-switches/
+open -a "Google Chrome" --args --make-default-browser
+
+echo "Chrome setup complete. Remember to load & configure custom extensions!"

356482fe3245a1de645222aecea4031bb801bdb0
Noah Hessel <noah.hessel@datasite.com>
2025-12-16 10:00:18 -0700

    Tweaks

--- chrome/install.sh
+++ chrome/install.sh
@@ -1,6 +1,8 @@
 echo "Setting up Chrome..."
 
 BROWSER_EXTENSIONS_DIR="$HOME/Documents/Repos/browser-extensions"
+rm -rf "$BROWSER_EXTENSIONS_DIR"
+mkdir -p "$BROWSER_EXTENSIONS_DIR"
 git clone git@github.com:MerrillCorporation/browser-extensions.git "$BROWSER_EXTENSIONS_DIR"
 killall "Google Chrome" &> /dev/null || echo "Chrome not running."
 open -a "Google Chrome" --args \

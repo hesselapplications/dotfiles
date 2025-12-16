1bf06281a56789043e5a1279e45bf92a8ca9dfc4
Noah Hessel <noah.hessel@datasite.com>
2025-12-16 14:06:47 -0700

    Tweaks

--- chrome/install.sh
+++ chrome/install.sh
@@ -5,7 +5,7 @@ rm -rf "$BROWSER_EXTENSIONS_DIR"
 mkdir -p "$BROWSER_EXTENSIONS_DIR"
 git clone git@github.com:MerrillCorporation/browser-extensions.git "$BROWSER_EXTENSIONS_DIR"
 
-https://peter.sh/experiments/chromium-command-line-switches/
+# https://peter.sh/experiments/chromium-command-line-switches/
 open -a "Google Chrome" --args --make-default-browser
 open -a "Google Chrome" chrome://extensions
 
